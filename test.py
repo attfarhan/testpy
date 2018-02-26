766dacb8479cc3f5dedca9eff49f02fa37e97651
Farhan Attamimi <farhan@sourcegraph.com>
2018-02-26 13:11:40 -0800

    0.6

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-from django.urls import path
-from libfarhan import testfunc
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
+from libfarhan.libfarhan import testfunc
 
 print(testfunc())
